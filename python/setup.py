1da39e807334964a72c412d3f00a31aff8fa8831
gdtrang <66646119+gdtrang@users.noreply.github.com>
2022-05-26 13:59:47 -0700

    Add setup.py as mentioned in graphframes#238
    Hoping to run this in Sagemaker Studio after:
    !pip install "git+https://github.com/PHPirates/graphframes.git@add-setup.py#egg=graphframes&subdirectory=python"

--- python/setup.py
+++ python/setup.py
@@ -1,2 +1,9 @@
 # Your python setup file. An example can be found at:
 # https://github.com/pypa/sampleproject/blob/master/setup.py
+from setuptools import setup
+
+setup(
+    name='graphframes',
+    version='0.8.2',
+    packages=['graphframes', 'graphframes.lib', 'graphframes.examples']
+)
